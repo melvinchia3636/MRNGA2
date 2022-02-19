9fb4ce8f81263d189f3942d5954bf5525a4caa83
melvinchia3636 <melvinchia623600@gmail.com>
2022-02-19 16:41:20 +0800

    exchange rate section completed

--- src/projects/CurrencyConverter/pages/exchangeRate.jsx
+++ src/projects/CurrencyConverter/pages/exchangeRate.jsx
@@ -1,23 +1,201 @@
-import { View, Text } from 'react-native';
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/prop-types */
+import {
+  View, Text, FlatList, TextInput, RefreshControl,
+} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import CountryFlag from 'react-native-country-flag';
+import { Feather } from '@expo/vector-icons';
+import moment from 'moment';
+import { FAB } from 'react-native-paper';
+import { AnimatePresence, MotiView } from 'moti';
 import scrapeData from '../scraper/exchangeRate';
+import { getISOByParam } from '../scraper/ISOCountry';
+import currencyName from '../scraper/currencyName.json';
+
+function RateItem({ item }) {
+  return (getISOByParam('currency', item[0]) !== undefined && currencyName.filter((e) => e.cc === item[0])[0] !== undefined ? (
+    <View style={{
+      marginVertical: 6,
+      flexDirection: 'row',
+      alignItems: 'center',
+    }}
+    >
+      <CountryFlag
+        style={{
+          borderRadius: 4,
+        }}
+        isoCode={getISOByParam('currency', item[0]).toLowerCase()}
+        size={48}
+      />
+      <View style={{
+        justifyContent: 'center',
+        marginLeft: 12,
+        flex: 1,
+      }}
+      >
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 16,
+            flex: 1,
+          }}
+          numberOfLines={1}
+        >
+          {currencyName.filter((e) => e.cc === item[0])[0].name}
+        </Text>
+        <Text style={{
+          color: '#EAB308',
+        }}
+        >
+          {item[0]}
+        </Text>
+      </View>
+      <Text style={{
+        color: '#EAB308',
+        fontWeight: 'bold',
+        marginLeft: 24,
+      }}
+      >
+        {item[1]}
+      </Text>
+    </View>
+  ) : null);
+}
 
 function ExchangeRate() {
   const [data, setData] = useState();
+  const [query, setQuery] = useState('');
+  const [refreshing, setRefreshing] = useState(false);
+  const [FABShow, setFABShow] = useState(false);
+  const listRef = useRef();
 
   useEffect(() => {
     scrapeData().then((d) => setData(d));
   }, []);
 
+  const updateData = () => {
+    scrapeData().then((d) => setData(d));
+    setRefreshing(false);
+  };
+
+  const listOnScroll = (e) => {
+    if (e.nativeEvent.contentOffset.y > 150) {
+      setFABShow(true);
+    } else {
+      setFABShow(false);
+    }
+  };
+
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: 'white',
-      padding: 24,
-    }}
-    >
-      <Text>{JSON.stringify(data)}</Text>
-    </View>
+    data ? (
+      <View style={{
+        flex: 1,
+        backgroundColor: 'white',
+        paddingHorizontal: 24,
+      }}
+      >
+        <View style={{
+          marginTop: 24,
+          marginBottom: 12,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+        >
+          <Text style={{
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: 18,
+          }}
+          >
+            1 USD =
+          </Text>
+          <Text>
+            Last updated:
+            {' '}
+            <Text style={{
+              color: '#EAB308',
+            }}
+            >
+              {moment(data.time_last_update_unix * 1000).format('Do MMMM YYYY')}
+            </Text>
+          </Text>
+        </View>
+        <View style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginBottom: 24,
+          backgroundColor: '#F1F5F9',
+          padding: 12,
+          borderRadius: 6,
+        }}
+        >
+          <Feather
+            style={{
+              marginRight: 8,
+            }}
+            name="search"
+            size={24}
+            color="black"
+          />
+          <TextInput
+            value={query}
+            onChangeText={setQuery}
+            selectionColor="#EAB308"
+            placeholder="Search for currency"
+          />
+        </View>
+        <FlatList
+          ref={listRef}
+          onScroll={listOnScroll}
+          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={updateData} />}
+          data={Object.entries(data.rates).filter((e) => (query === '' ? true : (
+            getISOByParam('currency', e[0])?.toLowerCase().includes(query.toLowerCase())
+            || currencyName.filter((c) => c.cc === e[0])[0]
+              ?.name
+              .toLowerCase()
+              .includes(query.toLowerCase())
+            || e[0].toLowerCase().includes(query.toLowerCase())
+          )))}
+          keyExtractor={(item) => item[0]}
+          renderItem={RateItem}
+          contentContainerStyle={{
+            paddingBottom: 24,
+          }}
+        />
+        <AnimatePresence>
+          {FABShow && (
+          <MotiView
+            from={{
+              bottom: -100,
+            }}
+            animate={{
+              bottom: 0,
+            }}
+            exit={{
+              bottom: -100,
+            }}
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              right: 0,
+              margin: 16,
+            }}
+          >
+            <FAB
+              style={{
+                backgroundColor: '#EAB308',
+              }}
+              onPress={() => listRef.current.scrollToOffset(0)}
+              icon={() => <Feather name="chevrons-up" size={24} color="white" />}
+            />
+          </MotiView>
+          )}
+        </AnimatePresence>
+      </View>
+    ) : null
   );
 }
 
---
+++ src/projects/CurrencyConverter/scraper/ISOCountry.js
@@ -0,0 +1,1038 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable consistent-return */
+/* eslint-disable func-names */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-syntax */
+const dataJSON = {
+  AD: {
+    countryName: 'Andorra',
+    currency: 'EUR',
+  },
+  AE: {
+    countryName: 'United Arab Emirates',
+    currency: 'AED',
+    symbol: 'AED',
+  },
+  AF: {
+    countryName: 'Afghanistan',
+    currency: 'AFN',
+  },
+  AG: {
+    countryName: 'Antigua and Barbuda',
+    currency: 'XCD',
+  },
+  AI: {
+    countryName: 'Anguilla',
+    currency: 'XCD',
+  },
+  AL: {
+    countryName: 'Albania',
+    currency: 'ALL',
+    symbol: 'ALL',
+  },
+  AM: {
+    countryName: 'Armenia',
+    currency: 'AMD',
+  },
+  AO: {
+    countryName: 'Angola',
+    currency: 'AOA',
+  },
+  AR: {
+    countryName: 'Argentina',
+    currency: 'ARS',
+    symbol: 'AR$',
+  },
+  AT: {
+    countryName: 'Austria',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  AU: {
+    countryName: 'Australia',
+    currency: 'AUD',
+    symbol: 'AU$',
+  },
+  AW: {
+    countryName: 'Aruba',
+    currency: 'AWG',
+  },
+  AX: {
+    countryName: 'Aland Islands',
+    currency: 'EUR',
+  },
+  AZ: {
+    countryName: 'Azerbaijan',
+    currency: 'AZN',
+  },
+  BA: {
+    countryName: 'Bosnia and Herzegovina',
+    currency: 'BAM',
+    symbol: 'KM',
+  },
+  BB: {
+    countryName: 'Barbados',
+    currency: 'BBD',
+  },
+  BD: {
+    countryName: 'Bangladesh',
+    currency: 'BDT',
+  },
+  BE: {
+    countryName: 'Belgium',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  BF: {
+    countryName: 'Burkina Faso',
+    currency: 'XOF',
+  },
+  BG: {
+    countryName: 'Bulgaria',
+    currency: 'BGN',
+    symbol: 'BGN',
+  },
+  BH: {
+    countryName: 'Bahrain',
+    currency: 'BHD',
+    symbol: 'BD',
+  },
+  BI: {
+    countryName: 'Burundi',
+    currency: 'BIF',
+  },
+  BJ: {
+    countryName: 'Benin',
+    currency: 'XOF',
+  },
+  BL: {
+    countryName: 'Saint Barthelemy',
+    currency: 'EUR',
+  },
+  BM: {
+    countryName: 'Bermuda',
+    currency: 'BMD',
+  },
+  BN: {
+    countryName: 'Brunei',
+    currency: 'BND',
+  },
+  BO: {
+    countryName: 'Bolivia',
+    currency: 'BOB',
+    symbol: 'Bs',
+  },
+  BR: {
+    countryName: 'Brazil',
+    currency: 'BRL',
+    symbol: 'R$',
+  },
+  BS: {
+    countryName: 'Bahamas',
+    currency: 'BSD',
+  },
+  BT: {
+    countryName: 'Bhutan',
+    currency: 'BTN',
+  },
+  BV: {
+    countryName: 'Bouvet Island',
+    currency: 'NOK',
+  },
+  BW: {
+    countryName: 'Botswana',
+    currency: 'BWP',
+  },
+  BY: {
+    countryName: 'Belarus',
+    currency: 'BYN',
+    symbol: 'BYN',
+  },
+  BZ: {
+    countryName: 'Belize',
+    currency: 'BZD',
+  },
+  CA: {
+    countryName: 'Canada',
+    currency: 'CAD',
+    symbol: 'CA$',
+  },
+  CC: {
+    countryName: 'Cocos Islands',
+    currency: 'AUD',
+  },
+  CD: {
+    countryName: 'Democratic Republic of the Congo',
+    currency: 'CDF',
+  },
+  CF: {
+    countryName: 'Central African Republic',
+    currency: 'XAF',
+  },
+  CG: {
+    countryName: 'Republic of the Congo',
+    currency: 'XAF',
+  },
+  CH: {
+    countryName: 'Switzerland',
+    currency: 'CHF',
+    symbol: 'CHF',
+  },
+  CI: {
+    countryName: 'Ivory Coast',
+    currency: 'XOF',
+  },
+  CK: {
+    countryName: 'Cook Islands',
+    currency: 'NZD',
+  },
+  CL: {
+    countryName: 'Chile',
+    currency: 'CLP',
+    symbol: 'CL$',
+  },
+  CM: {
+    countryName: 'Cameroon',
+    currency: 'XAF',
+  },
+  CN: {
+    countryName: 'China',
+    currency: 'CNY',
+    symbol: 'CN¥',
+  },
+  CO: {
+    countryName: 'Colombia',
+    currency: 'COP',
+    symbol: 'CO$',
+  },
+  CR: {
+    countryName: 'Costa Rica',
+    currency: 'CRC',
+    symbol: '₡',
+  },
+  CU: {
+    countryName: 'Cuba',
+    currency: 'CUP',
+  },
+  CV: {
+    countryName: 'Cape Verde',
+    currency: 'CVE',
+  },
+  CW: {
+    countryName: 'Curacao',
+    currency: 'ANG',
+  },
+  CX: {
+    countryName: 'Christmas Island',
+    currency: 'AUD',
+  },
+  CY: {
+    countryName: 'Cyprus',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  CZ: {
+    countryName: 'Czech Republic',
+    currency: 'CZK',
+    symbol: 'Kč',
+  },
+  DE: {
+    countryName: 'Germany',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  DJ: {
+    countryName: 'Djibouti',
+    currency: 'DJF',
+  },
+  DK: {
+    countryName: 'Denmark',
+    currency: 'DKK',
+    symbol: 'Dkr',
+  },
+  DM: {
+    countryName: 'Dominica',
+    currency: 'XCD',
+  },
+  DO: {
+    countryName: 'Dominican Republic',
+    currency: 'DOP',
+    symbol: 'RD$',
+  },
+  DZ: {
+    countryName: 'Algeria',
+    currency: 'DZD',
+    symbol: 'DA',
+  },
+  EE: {
+    countryName: 'Estonia',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  EG: {
+    countryName: 'Egypt',
+    currency: 'EGP',
+    symbol: 'EGP',
+  },
+  EH: {
+    countryName: 'Western Sahara',
+    currency: 'MAD',
+  },
+  ER: {
+    countryName: 'Eritrea',
+    currency: 'ERN',
+  },
+  ES: {
+    countryName: 'Spain',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  ET: {
+    countryName: 'Ethiopia',
+    currency: 'ETB',
+  },
+  FI: {
+    countryName: 'Finland',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  FJ: {
+    countryName: 'Fiji',
+    currency: 'FJD',
+  },
+  FK: {
+    countryName: 'Falkland Islands',
+    currency: 'FKP',
+  },
+  FO: {
+    countryName: 'Faroe Islands',
+    currency: 'DKK',
+  },
+  FR: {
+    countryName: 'France',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  GA: {
+    countryName: 'Gabon',
+    currency: 'XAF',
+  },
+  GB: {
+    countryName: 'United Kingdom',
+    currency: 'GBP',
+    symbol: '£',
+  },
+  GD: {
+    countryName: 'Grenada',
+    currency: 'XCD',
+  },
+  GE: {
+    countryName: 'Georgia',
+    currency: 'GEL',
+  },
+  GF: {
+    countryName: 'French Guiana',
+    currency: 'EUR',
+  },
+  GG: {
+    countryName: 'Guernsey',
+    currency: 'GBP',
+  },
+  GH: {
+    countryName: 'Ghana',
+    currency: 'GHS',
+  },
+  GI: {
+    countryName: 'Gibraltar',
+    currency: 'GIP',
+  },
+  GL: {
+    countryName: 'Greenland',
+    currency: 'DKK',
+  },
+  GM: {
+    countryName: 'Gambia',
+    currency: 'GMD',
+  },
+  GN: {
+    countryName: 'Guinea',
+    currency: 'GNF',
+  },
+  GP: {
+    countryName: 'Guadeloupe',
+    currency: 'EUR',
+  },
+  GQ: {
+    countryName: 'Equatorial Guinea',
+    currency: 'XAF',
+  },
+  GR: {
+    countryName: 'Greece',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  GS: {
+    countryName: 'South Georgia and the South Sandwich Islands',
+    currency: 'GBP',
+  },
+  GT: {
+    countryName: 'Guatemala',
+    currency: 'GTQ',
+    symbol: 'GTQ',
+  },
+  GW: {
+    countryName: 'Guinea-Bissau',
+    currency: 'XOF',
+  },
+  GY: {
+    countryName: 'Guyana',
+    currency: 'GYD',
+  },
+  HK: {
+    countryName: 'Hong Kong',
+    currency: 'HKD',
+    symbol: 'HK$',
+  },
+  HM: {
+    countryName: 'Heard Island and McDonald Islands',
+    currency: 'AUD',
+  },
+  HN: {
+    countryName: 'Honduras',
+    currency: 'HNL',
+    symbol: 'HNL',
+  },
+  HR: {
+    countryName: 'Croatia',
+    currency: 'HRK',
+    symbol: 'kn',
+  },
+  HT: {
+    countryName: 'Haiti',
+    currency: 'HTG',
+  },
+  HU: {
+    countryName: 'Hungary',
+    currency: 'HUF',
+    symbol: 'Ft',
+  },
+  ID: {
+    countryName: 'Indonesia',
+    currency: 'IDR',
+    symbol: 'Rp',
+  },
+  IE: {
+    countryName: 'Ireland',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  IL: {
+    countryName: 'Israel',
+    currency: 'ILS',
+    symbol: '₪',
+  },
+  IM: {
+    countryName: 'Isle of Man',
+    currency: 'GBP',
+  },
+  IN: {
+    countryName: 'India',
+    currency: 'INR',
+    symbol: 'Rs',
+  },
+  IQ: {
+    countryName: 'Iraq',
+    currency: 'IQD',
+    symbol: 'IQD',
+  },
+  IR: {
+    countryName: 'Iran',
+    currency: 'IRR',
+  },
+  IS: {
+    countryName: 'Iceland',
+    currency: 'ISK',
+    symbol: 'Ikr',
+  },
+  IT: {
+    countryName: 'Italy',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  JE: {
+    countryName: 'Jersey',
+    currency: 'GBP',
+  },
+  JM: {
+    countryName: 'Jamaica',
+    currency: 'JMD',
+  },
+  JO: {
+    countryName: 'Jordan',
+    currency: 'JOD',
+    symbol: 'JD',
+  },
+  JP: {
+    countryName: 'Japan',
+    currency: 'JPY',
+    symbol: '¥',
+  },
+  KE: {
+    countryName: 'Kenya',
+    currency: 'KES',
+  },
+  KG: {
+    countryName: 'Kyrgyzstan',
+    currency: 'KGS',
+  },
+  KH: {
+    countryName: 'Cambodia',
+    currency: 'KHR',
+  },
+  KI: {
+    countryName: 'Kiribati',
+    currency: 'AUD',
+  },
+  KM: {
+    countryName: 'Comoros',
+    currency: 'KMF',
+  },
+  KN: {
+    countryName: 'Saint Kitts and Nevis',
+    currency: 'XCD',
+  },
+  KP: {
+    countryName: 'North Korea',
+    currency: 'KPW',
+  },
+  KR: {
+    countryName: 'South Korea',
+    currency: 'KRW',
+    symbol: '₩',
+  },
+  KW: {
+    countryName: 'Kuwait',
+    currency: 'KWD',
+    symbol: 'KD',
+  },
+  KY: {
+    countryName: 'Cayman Islands',
+    currency: 'KYD',
+  },
+  KZ: {
+    countryName: 'Kazakhstan',
+    currency: 'KZT',
+  },
+  LA: {
+    countryName: 'Laos',
+    currency: 'LAK',
+  },
+  LB: {
+    countryName: 'Lebanon',
+    currency: 'LBP',
+    symbol: 'LB£',
+  },
+  LC: {
+    countryName: 'Saint Lucia',
+    currency: 'XCD',
+  },
+  LI: {
+    countryName: 'Liechtenstein',
+    currency: 'CHF',
+  },
+  LK: {
+    countryName: 'Sri Lanka',
+    currency: 'LKR',
+  },
+  LR: {
+    countryName: 'Liberia',
+    currency: 'LRD',
+  },
+  LS: {
+    countryName: 'Lesotho',
+    currency: 'LSL',
+  },
+  LT: {
+    countryName: 'Lithuania',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  LU: {
+    countryName: 'Luxembourg',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  LV: {
+    countryName: 'Latvia',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  LY: {
+    countryName: 'Libya',
+    currency: 'LYD',
+    symbol: 'LD',
+  },
+  MA: {
+    countryName: 'Morocco',
+    currency: 'MAD',
+    symbol: 'MAD',
+  },
+  MC: {
+    countryName: 'Monaco',
+    currency: 'EUR',
+  },
+  MD: {
+    countryName: 'Moldova',
+    currency: 'MDL',
+  },
+  ME: {
+    countryName: 'Montenegro',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  MF: {
+    countryName: 'Saint Martin',
+    currency: 'EUR',
+  },
+  MG: {
+    countryName: 'Madagascar',
+    currency: 'MGA',
+  },
+  MK: {
+    countryName: 'Macedonia',
+    currency: 'MKD',
+    symbol: 'MKD',
+  },
+  ML: {
+    countryName: 'Mali',
+    currency: 'XOF',
+  },
+  MM: {
+    countryName: 'Myanmar',
+    currency: 'MMK',
+  },
+  MN: {
+    countryName: 'Mongolia',
+    currency: 'MNT',
+  },
+  MO: {
+    countryName: 'Macao',
+    currency: 'MOP',
+  },
+  MQ: {
+    countryName: 'Martinique',
+    currency: 'EUR',
+  },
+  MR: {
+    countryName: 'Mauritania',
+    currency: 'MRU',
+  },
+  MS: {
+    countryName: 'Montserrat',
+    currency: 'XCD',
+  },
+  MT: {
+    countryName: 'Malta',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  MU: {
+    countryName: 'Mauritius',
+    currency: 'MUR',
+  },
+  MV: {
+    countryName: 'Maldives',
+    currency: 'MVR',
+  },
+  MW: {
+    countryName: 'Malawi',
+    currency: 'MWK',
+  },
+  MX: {
+    countryName: 'Mexico',
+    currency: 'MXN',
+    symbol: 'MX$',
+  },
+  MY: {
+    countryName: 'Malaysia',
+    currency: 'MYR',
+    symbol: 'RM',
+  },
+  MZ: {
+    countryName: 'Mozambique',
+    currency: 'MZN',
+  },
+  NA: {
+    countryName: 'Namibia',
+    currency: 'NAD',
+  },
+  NC: {
+    countryName: 'New Caledonia',
+    currency: 'XPF',
+  },
+  NE: {
+    countryName: 'Niger',
+    currency: 'XOF',
+  },
+  NF: {
+    countryName: 'Norfolk Island',
+    currency: 'AUD',
+  },
+  NG: {
+    countryName: 'Nigeria',
+    currency: 'NGN',
+  },
+  NI: {
+    countryName: 'Nicaragua',
+    currency: 'NIO',
+    symbol: 'C$',
+  },
+  NL: {
+    countryName: 'Netherlands',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  NO: {
+    countryName: 'Norway',
+    currency: 'NOK',
+    symbol: 'Nkr',
+  },
+  NP: {
+    countryName: 'Nepal',
+    currency: 'NPR',
+  },
+  NR: {
+    countryName: 'Nauru',
+    currency: 'AUD',
+  },
+  NU: {
+    countryName: 'Niue',
+    currency: 'NZD',
+  },
+  NZ: {
+    countryName: 'New Zealand',
+    currency: 'NZD',
+    symbol: 'NZ$',
+  },
+  OM: {
+    countryName: 'Oman',
+    currency: 'OMR',
+    symbol: 'OMR',
+  },
+  PA: {
+    countryName: 'Panama',
+    currency: 'PAB',
+    symbol: 'B/.',
+  },
+  PE: {
+    countryName: 'Peru',
+    currency: 'PEN',
+    symbol: 'S/.',
+  },
+  PF: {
+    countryName: 'French Polynesia',
+    currency: 'XPF',
+  },
+  PG: {
+    countryName: 'Papua New Guinea',
+    currency: 'PGK',
+  },
+  PH: {
+    countryName: 'Philippines',
+    currency: 'PHP',
+    symbol: '₱',
+  },
+  PK: {
+    countryName: 'Pakistan',
+    currency: 'PKR',
+  },
+  PL: {
+    countryName: 'Poland',
+    currency: 'PLN',
+    symbol: 'zł',
+  },
+  PM: {
+    countryName: 'Saint Pierre and Miquelon',
+    currency: 'EUR',
+  },
+  PN: {
+    countryName: 'Pitcairn',
+    currency: 'NZD',
+  },
+  PS: {
+    countryName: 'Palestinian Territory',
+    currency: 'ILS',
+  },
+  PT: {
+    countryName: 'Portugal',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  PY: {
+    countryName: 'Paraguay',
+    currency: 'PYG',
+    symbol: '₲',
+  },
+  QA: {
+    countryName: 'Qatar',
+    currency: 'QAR',
+    symbol: 'QR',
+  },
+  RE: {
+    countryName: 'Reunion',
+    currency: 'EUR',
+  },
+  RO: {
+    countryName: 'Romania',
+    currency: 'RON',
+    symbol: 'RON',
+  },
+  RS: {
+    countryName: 'Serbia',
+    currency: 'RSD',
+    symbol: 'din.',
+  },
+  RU: {
+    countryName: 'Russia',
+    currency: 'RUB',
+    symbol: 'RUB',
+  },
+  RW: {
+    countryName: 'Rwanda',
+    currency: 'RWF',
+  },
+  SA: {
+    countryName: 'Saudi Arabia',
+    currency: 'SAR',
+    symbol: 'SR',
+  },
+  SB: {
+    countryName: 'Solomon Islands',
+    currency: 'SBD',
+  },
+  SC: {
+    countryName: 'Seychelles',
+    currency: 'SCR',
+  },
+  SD: {
+    countryName: 'Sudan',
+    currency: 'SDG',
+    symbol: 'SDG',
+  },
+  SE: {
+    countryName: 'Sweden',
+    currency: 'SEK',
+    symbol: 'Skr',
+  },
+  SG: {
+    countryName: 'Singapore',
+    currency: 'SGD',
+    symbol: 'S$',
+  },
+  SH: {
+    countryName: 'Saint Helena',
+    currency: 'SHP',
+  },
+  SI: {
+    countryName: 'Slovenia',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  SJ: {
+    countryName: 'Svalbard and Jan Mayen',
+    currency: 'NOK',
+  },
+  SK: {
+    countryName: 'Slovakia',
+    currency: 'EUR',
+    symbol: '€',
+  },
+  SL: {
+    countryName: 'Sierra Leone',
+    currency: 'SLL',
+  },
+  SM: {
+    countryName: 'San Marino',
+    currency: 'EUR',
+  },
+  SN: {
+    countryName: 'Senegal',
+    currency: 'XOF',
+  },
+  SO: {
+    countryName: 'Somalia',
+    currency: 'SOS',
+  },
+  SR: {
+    countryName: 'Suriname',
+    currency: 'SRD',
+  },
+  SS: {
+    countryName: 'South Sudan',
+    currency: 'SSP',
+  },
+  ST: {
+    countryName: 'Sao Tome and Principe',
+    currency: 'STD',
+  },
+  SX: {
+    countryName: 'Sint Maarten',
+    currency: 'ANG',
+  },
+  SY: {
+    countryName: 'Syria',
+    currency: 'SYP',
+    symbol: 'SY£',
+  },
+  SZ: {
+    countryName: 'Swaziland',
+    currency: 'SZL',
+  },
+  TD: {
+    countryName: 'Chad',
+    currency: 'XAF',
+  },
+  TF: {
+    countryName: 'French Southern Territories',
+    currency: 'EUR',
+  },
+  TG: {
+    countryName: 'Togo',
+    currency: 'XOF',
+  },
+  TH: {
+    countryName: 'Thailand',
+    currency: 'THB',
+    symbol: '฿',
+  },
+  TJ: {
+    countryName: 'Tajikistan',
+    currency: 'TJS',
+  },
+  TK: {
+    countryName: 'Tokelau',
+    currency: 'NZD',
+  },
+  TM: {
+    countryName: 'Turkmenistan',
+    currency: 'TMT',
+  },
+  TN: {
+    countryName: 'Tunisia',
+    currency: 'TND',
+    symbol: 'DT',
+  },
+  TO: {
+    countryName: 'Tonga',
+    currency: 'TOP',
+  },
+  TR: {
+    countryName: 'Turkey',
+    currency: 'TRY',
+    symbol: 'TL',
+  },
+  TT: {
+    countryName: 'Trinidad and Tobago',
+    currency: 'TTD',
+  },
+  TV: {
+    countryName: 'Tuvalu',
+    currency: 'AUD',
+  },
+  TW: {
+    countryName: 'Taiwan',
+    currency: 'TWD',
+    symbol: 'NT$',
+  },
+  TZ: {
+    countryName: 'Tanzania',
+    currency: 'TZS',
+  },
+  UA: {
+    countryName: 'Ukraine',
+    currency: 'UAH',
+    symbol: '₴',
+  },
+  UG: {
+    countryName: 'Uganda',
+    currency: 'UGX',
+  },
+  US: {
+    countryName: 'United States',
+    currency: 'USD',
+    symbol: '$',
+  },
+  UY: {
+    countryName: 'Uruguay',
+    currency: 'UYU',
+    symbol: '$U',
+  },
+  UZ: {
+    countryName: 'Uzbekistan',
+    currency: 'UZS',
+  },
+  VA: {
+    countryName: 'Vatican',
+    currency: 'EUR',
+  },
+  VC: {
+    countryName: 'Saint Vincent and the Grenadines',
+    currency: 'XCD',
+  },
+  VE: {
+    countryName: 'Venezuela',
+    currency: 'VEF',
+    symbol: 'Bs.F.',
+  },
+  VN: {
+    countryName: 'Vietnam',
+    currency: 'VND',
+    symbol: '₫',
+  },
+  VU: {
+    countryName: 'Vanuatu',
+    currency: 'VUV',
+  },
+  WF: {
+    countryName: 'Wallis and Futuna',
+    currency: 'XPF',
+  },
+  WS: {
+    countryName: 'Samoa',
+    currency: 'WST',
+  },
+  XK: {
+    countryName: 'Kosovo',
+    currency: 'EUR',
+  },
+  YE: {
+    countryName: 'Yemen',
+    currency: 'YER',
+    symbol: 'YR',
+  },
+  YT: {
+    countryName: 'Mayotte',
+    currency: 'EUR',
+  },
+  ZA: {
+    countryName: 'South Africa',
+    currency: 'ZAR',
+    symbol: 'R',
+  },
+  ZM: {
+    countryName: 'Zambia',
+    currency: 'ZMW',
+  },
+  ZW: {
+    countryName: 'Zimbabwe',
+    currency: 'ZWL',
+  },
+};
+
+exports.getISOByParam = function (param, value) {
+  for (const key in dataJSON) {
+    if (dataJSON.hasOwnProperty(key) && dataJSON[key][param] === value) {
+      return key;
+    }
+  }
+};
